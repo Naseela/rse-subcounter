c578cc8db418f4f0f77bd9e6b05860de0c7093c1
Naseela Amboorallee <ad.amboorallee@gmail.com>
2018-02-15 11:37:18 +0000

    Create a plotting function that works for both data frames.

--- subcounter.py
+++ subcounter.py
@@ -17,6 +17,54 @@ import numpy as np
 # Create a data frame from the csv file.
 df = pd.read_csv("RSE_members.csv")
 
+
+def plotgraph(df_plot,file,title,xvalue,yvalue):
+    """
+    Creare and export a plot of the dataframe.
+    :params: dataframe, file name, graph title, data frame columns
+    :return: a png file of the plotted graph
+    """
+
+
+    # Created variables needed for plotting.
+    fig, ax = plt.subplots()
+    x = df_plot[xvalue]
+    y = df_plot[yvalue]
+
+    # Create a new list with the desired tick labels.
+    xticklist = x.tolist()
+    print(xticklist)
+    xticklist = xticklist[0::6]
+    print(xticklist)
+
+    # Plot data into line graph with setttings
+    ax.plot(x,y)
+
+    # Auto format the date labels on the x axis.
+    fig.autofmt_xdate()
+
+    # Set the title and labels for the graph.
+    ax.set_title(title)
+    ax.set_xlabel(xvalue)
+    ax.set_ylabel(yvalue)
+
+    # Set major and minor ticks for thhe x axis.
+    start, end = ax.get_xlim()
+    major_ticks = np.arange(start+2, end,6)
+    minor_ticks = np.arange(start,end)
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+
+    # Set the tick labels as the list with the desired scale.
+    ax.set_xticklabels(xticklist)
+
+    # Ensure the layout displays correctly.
+    plt.tight_layout()
+
+    # Export to png file.
+    plt.savefig(file+".png")
+
+
 # Format the date column
 # Create a new column where the dates contain only the month and year.
 # Map function lambda which removes preceeding numerical values from sub dates.
@@ -28,78 +76,28 @@ df["Month"]=df["Month"].dt.strftime('%Y/%m')
 
 # Count the subscriptions per month.
 # Group months and create new column from the amount of times the group appears.
-df["Sub count"] = df.groupby(df["Month"])["Month"].transform('count')
+df["Subscriptions"] = df.groupby(df["Month"])["Month"].transform('count')
 
 # Removes all duplicate rows from the data frame.
 df.drop_duplicates(subset="Month", inplace=True)
 
 # Create a new data frame containing only the necessary columns.
-subdf = df[["Month","Sub count"]].copy()
-
+subdf = df[["Month","Subscriptions"]].copy()
 print (subdf)
+
 # Export the new data frame into a csv file.
 subdf.to_csv('Subscription_count.csv')
 
-
-
-# Created variables needed for plotting.
-fig, ax = plt.subplots()
-x = subdf["Month"]
-y = subdf["Sub count"]
-
-# Create a new list with the desired tick labels.
-xticklist = x.tolist()
-print(xticklist)
-xticklist = xticklist[0::6]
-print(xticklist)
-
-
-# Plot data into line graph with setttings
-ax.plot(x,y)
-
-# Auto format the date labels on the x axis.
-fig.autofmt_xdate()
-
-# Set the title and labels for the graph.
-ax.set_title("Subscriptions per Month")
-ax.set_xlabel("Month")
-ax.set_ylabel("Subscriptions")
-
-# Set major and minor ticks for thhe x axis.
-start, end = ax.get_xlim()
-major_ticks = np.arange(start+2, end,6)
-minor_ticks = np.arange(start,end)
-ax.set_xticks(major_ticks)
-ax.set_xticks(minor_ticks, minor=True)
-
-# Set the tick labels as the list with the desired scale.
-ax.set_xticklabels(xticklist)
-
-# Ensure the layout displays correctly.
-plt.tight_layout()
-
-# Export to png file.
-plt.savefig("Subscription_count.png")
+# Plot the count graph.
+plotgraph(subdf,"Subscription_count","Subscriptions per Month","Month", "Subscriptions")
 
 # Create a new column for the cumulative sum in the sub data frame.
-subdf["Total"]= subdf["Sub count"].cumsum()
+subdf["Total Subscribers"]= subdf["Subscriptions"].cumsum()
 
 # Create a new data frame from the sub data frame with desire columns.
-cumdf = subdf[["Month","Total"]].copy()
-
-
-# Plot a graph of the total subscribers.
-cumdf.plot(x="Month",y="Total",kind='line', title="Total Subscribers",legend=False,color="b")
-
-
-# Format the graph and axis.
-plt.xticks(range(len(cumdf)),cumdf["Month"],size="small",rotation=270)
-plt.xlabel("Month")
-plt.ylabel("Total Subscribers")
-
-plt.tight_layout()
-
-# Export data frame into image.
-plt.savefig("Total_subs.png")
-
+cumdf = subdf[["Month","Total Subscribers"]].copy()
 print(cumdf)
+
+
+# Plot the cumulative sum graph.
+plotgraph(cumdf,"Total_subs","Total Subscribers","Month","Total Subscribers")
